2b97d1ad13c8a166033805640da4c03e9d4c16ea
Alex Brewer <abrewer@forallsecure.com>
2022-04-19 13:58:26 -0400

    Create example that classifies gRPC status codes and raises issues on 5XX responses.

--- plugins/python-classify-plugin/src/plugin.py
+++ plugins/python-classify-plugin/src/plugin.py
@@ -4,17 +4,66 @@ import sys
 import os
 from concurrent import futures
 import grpc
+import numpy as np
 
 sys.path.append("generated")
 from response_classify_plugin_pb2 import Response, Issues
 from response_classify_plugin_pb2_grpc import ClassifyPluginServicer, add_ClassifyPluginServicer_to_server
 
+
+# Based on this info: https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto
+
+GrpcToHTTPDict = {
+    '0':'200',
+    '1':'499',
+    '2':'500',
+    '3':'400',
+    '4':'504',
+    '5':'404',
+    '6':'409',
+    '7':'403',
+    '8':'429',
+    '9':'400',
+    '10':'409',
+    '11':'400',
+    '12':'501',
+    '13':'500',
+    '14':'503',
+    '15':'500',
+    '16':'401'
+}
+
+HTTPToProblemDict = {
+    '200':'OK',
+    '499':'Canceled',
+    '500':'UnknownError',
+    '400':'InvalidArgument',
+    '504':'DeadlineExceeded',
+    '404':'NotFound',
+    '409':'AlreadyExists',
+    '403':'PermissionDenied',
+    '429':'ResourceExhausted',
+    '400':'FailedPrecondition',
+    '409':'Aborted',
+    '400':'OutOfRange',
+    '501':'Unimplemented',
+    '500':'InternalError',
+    '503':'Unavailable',
+    '500':'DataLoss',
+    '401':'Unauthenticated'
+}
+
 ## implement the ClassifyPlugin interface
 class ClassifyPluginServicer(ClassifyPluginServicer):
     def Classify(self, response, context):
-        print(response.body)
-        print(response.status)
+        body = str(response.body)
+        grpcStatusStart = body.find('grpc-status:') + len('grpc-status:')
+        grpcStatusEnd = body.find('\\', grpcStatusStart)
+        grpcStatus = body[grpcStatusStart:grpcStatusEnd]
+        HTTPResponse = GrpcToHTTPDict[grpcStatus]
         issues = Issues()
+        if HTTPResponse[:1] == '5':
+            issues.issues.append(Issues.Issue(summary=str(HTTPResponse + ': ' + HTTPToProblemDict[HTTPResponse])))
         return issues
 
 def get_port():
